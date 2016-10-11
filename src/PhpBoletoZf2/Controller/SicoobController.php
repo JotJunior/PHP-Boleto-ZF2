5e6b9e670c0b3d376e2dc133c854d2e1d49f3003
Matheus Ferreira <matheus@versatecnologia.com.br>
2016-10-11 10:36:42 -0300

    dev: inserindo logo do sicoob e controller

---
+++ src/PhpBoletoZf2/Controller/SicoobController.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * PHP Boleto ZF2 - Versão Beta 
+ * 
+ * Este arquivo está disponível sob a Licença GPL disponível pela Web
+ * em http://pt.wikipedia.org/wiki/GNU_General_Public_License 
+ * Você deve ter recebido uma cópia da GNU Public License junto com
+ * este pacote; se não, escreva para: 
+ * 
+ * Free Software Foundation, Inc.
+ * 59 Temple Place - Suite 330
+ * Boston, MA 02111-1307, USA.
+ * 
+ * Originado do Projeto BoletoPhp: http://www.boletophp.com.br 
+ * 
+ * Adaptação ao Zend Framework 2: Matheus Ferreira S. <santana.matheusferreira@gmail.com>
+ * 
+ */
+
+namespace PhpBoletoZf2\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+use DOMPDFModule\View\Model\PdfModel;
+use PhpBoletoZf2\Model\BoletoSicoob;
+use PhpBoletoZf2\Model\Sacado;
+
+class SicoobController extends AbstractActionController
+{
+    public function indexAction()
+    {
+        /**
+         * Definindo o layout padrão do boleto
+         */
+        $this->layout('layout/boleto');
+
+        $request = $this->getRequest();
+
+        $form = new \PhpBoletoZf2\Form\Boleto;
+
+        if ($request->isPost()) {
+            $form->setData($request->getPost());
+
+            if ($form->isValid()) {
+                $data = $form->getData();
+
+                $boleto = new BoletoCaixa($data);
+                $sacado = new Sacado($data);
+
+                $caixa = $this->getServiceLocator()
+                        ->get('Boleto\CaixaSigcb');
+                $caixa->setSacado($sacado)
+                        ->setBoleto($boleto);
+
+                $dados = $caixa->prepare();
+            }
+        }
+    }
+}
